eb71efea7ffa1ded75120ea73d1226eef4b05d50
jodongha <jodongrami@gmail.com>
2023-01-26 13:32:16 +0900

    ADD : admin init

--- back/src/router/adminRouter.ts
+++ back/src/router/adminRouter.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 import * as adminController from "../controller/adminController";
-// import { adminJWT } from '../middleware/jwtValidator';
+import { adminJWT } from '../middleware/jwtValidator';
 
 const router: Router = express.Router();
 
-router.post("/admin/book", adminController.addBook);
+router.post("/book", adminJWT, adminController.addBook);
+
+router.put("/book/:id", adminJWT, adminController.updateBook);
 
 module.exports = router;
